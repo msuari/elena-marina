beeb6ae01c6bf436a6e39a4efcfcd557baecf696
Marina Suari <marina@piperai.com>
2025-03-30 21:34:48 +0200

    publish github

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  // Replace with your GitHub username and repository name
+  site: 'https://msuari.github.io',
+  base: '/elena-marina',
   vite: {
     plugins: [tailwindcss()]
   }
